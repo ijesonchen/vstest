a1bf2e9b9d2ef6e2aa9ad29ae3c67a737486a524
Jeson Chen <jetstorm@163.com>
2017-06-18 20:01:26 +0800

    w

--- VS2013CPP/StanfordAlg/wk5-SCC.cpp
+++ VS2013CPP/StanfordAlg/wk5-SCC.cpp
@@ -32,12 +32,17 @@ this on the discussion forums.
 #include <list>
 #include <fstream>
 #include <queue>
+#include <deque>
 #include <stack>
+#include <ctime>
 #include "common.h"
 
 using namespace std;
 #pragma warning(disable: 4018)
-// vertext index is 1-based
+
+//////////////////////////////////////////////////////////////////////////
+// vertex index is 1-based by default
+// 0BASE function is 0 based input / output, but proc as 1 based.
 
 
 class AdjGraph
@@ -119,6 +124,13 @@ bool operator!=(const AdjGraph& lhs, const AdjGraph& rhs)
 {
 	return !(lhs == rhs);
 }
+
+struct SnapShot
+{
+	int vertex = -1;
+	int stage = 0;
+};
+
 class DirectedDFS
 {
 public:
@@ -194,19 +206,99 @@ public:
 private:
 	void dfs(const AdjGraph& g, int s)
 	{
+		dfsStack(g, s);
+	}
+
+	void dfsStack(const AdjGraph& g, int s)
+	{
+		stack<SnapShot> stSnapShot;
+
+		SnapShot sSS;
+		sSS.vertex = s;
+		sSS.stage = 0;
+
+		SnapShot currentStack;
+		currentStack.vertex = -1;
+		currentStack.stage = -1;
+		auto currentV = currentStack.vertex;
+
+		stSnapShot.push(sSS);
+
+		while (!stSnapShot.empty())
+		{
+			currentStack = stSnapShot.top();
+			stSnapShot.pop();
+			currentV = currentStack.vertex;
+
+			switch (currentStack.stage)
+			{
+			case 0:
+			{
+				if (marked[currentV])
+				{
+					continue;
+				}
+				pre.push(currentV);
+				marked[currentV] = true;
+				stacked[currentV] = true;
+
+				currentStack.stage = 1;
+				stSnapShot.push(currentStack);
+
+				stack<int> sAdj;
+				for (const auto i : g.Adj(currentV))
+				{
+					sAdj.push(i);
+				}
+				while (!sAdj.empty())
+				{
+					auto i = sAdj.top();
+					sAdj.pop();
+					if (stacked[i])
+					{
+						cycled = true;
+					}
+					if (!marked[i])
+					{
+						// recur
+						SnapShot newSS;
+						newSS.vertex = i;
+						newSS.stage = 0;
+						stSnapShot.push(newSS);
+					}
+				}
+
+			}
+				break;
+			case 1:
+
+				stacked[currentV] = false;
+				post.push(currentV);
+				revPost.push(currentV);
+
+				break;
+			default:
+				abort();
+				break;
+			}
+		}
+	}
+
+	void dfsRecur(const AdjGraph& g, int s)
+	{
 		pre.push(s);
 
 		marked[s] = true;
 		stacked[s] = true;
 		for (const auto i : g.Adj(s))
 		{
-			if (!marked[i])
-			{
-				dfs(g, i);
-			}
 			if (stacked[i])
 			{
 				cycled = true;
+			}
+			if (!marked[i])
+			{
+				dfsRecur(g, i);
 			}
 		}
 		stacked[s] = false;
@@ -223,12 +315,114 @@ private:
 	stack<int> revPost;
 };
 
-// class KosarajuSCC
-// {
-// public:
-// 	KosarajuSCC(AdjGraph g)
-// private:
-// };
+class KosarajuSCC
+{
+public:
+	KosarajuSCC(const AdjGraph& g, vector<int> seq)
+	{
+		marked.assign(g.V() + 1, false);
+		scc.assign(g.V() + 1, -1);
+		for (auto i : seq)
+		{
+			if (!marked[i])
+			{
+				dfs(g, i);
+				++idx;
+			}
+		}
+	}
+
+	int Count(void) const { return idx; };
+	int Idx(int v) const { return scc[v]; };
+
+private:
+	void dfs(const AdjGraph& g, int s)
+	{
+		dfsStack(g, s);
+	}
+	void dfsStack(const AdjGraph& g, int s)
+	{
+		stack<SnapShot> stSnapShot;
+
+		SnapShot sSS;
+		sSS.vertex = s;
+		sSS.stage = 0;
+
+		SnapShot currentStack;
+		currentStack.vertex = -1;
+		currentStack.stage = -1;
+		auto currentV = currentStack.vertex;
+
+		stSnapShot.push(sSS);
+
+		while (!stSnapShot.empty())
+		{
+			currentStack = stSnapShot.top();
+			stSnapShot.pop();
+			currentV = currentStack.vertex;
+
+			switch (currentStack.stage)
+			{
+			case 0:
+			{
+				if (marked[currentV])
+				{
+					continue;
+				}
+				marked[currentV] = true;
+				scc[currentV] = idx;
+
+				currentStack.stage = 1;
+				stSnapShot.push(currentStack);
+
+				stack<int> sAdj;
+				for (const auto i : g.Adj(currentV))
+				{
+					sAdj.push(i);
+				}
+				while (!sAdj.empty())
+				{
+					auto i = sAdj.top();
+					sAdj.pop();
+					if (!marked[i])
+					{
+						// recur
+						SnapShot newSS;
+						newSS.vertex = i;
+						newSS.stage = 0;
+						stSnapShot.push(newSS);
+					}
+				}
+
+			}
+			break;
+			case 1:
+
+				break;
+			default:
+				abort();
+				break;
+			}
+		}
+	}
+
+
+	void dfsRecur(const AdjGraph& g, int s)
+	{
+		marked[s] = true;
+		scc[s] = idx;
+		for (const auto i : g.Adj(s))
+		{
+			if (!marked[i])
+			{
+				dfsRecur(g, i);
+			}
+		}
+	}
+	int idx = 0;
+	vector<bool> marked;
+	vector<int> scc;
+};
 
 AdjGraph ReadGraph0Base(const string& fileName, int nVertices)
 {
@@ -242,6 +436,28 @@ AdjGraph ReadGraph0Base(const string& fileName, int nVertices)
 	while (f >> x)
 	{
 		++x;
+		second = !second;
+		if (second)
+		{
+			g.AddEdge(u, x);
+			continue;
+		}
+		u = x;
+	}
+	return g;
+}
+
+AdjGraph ReadGraph(const string& fileName, int nVertices)
+{
+	AdjGraph g(nVertices);
+
+	fstream f(fileName, ios::in);
+	if (!f) { abort(); }
+	int x = 0;
+	int u = 0;
+	bool second = true;
+	while (f >> x)
+	{
 		second = !second;
 		if (second)
 		{
@@ -287,7 +503,21 @@ void GraphRev0Base(void)
 		g3.Print0Base();
 		abort();
 	}
-	cout << "reverse passed." << endl;
+	cout << "GraphRev0Base passed." << endl;
+}
+
+void GraphRevDG0Base(void)
+{
+	auto g1 = ReadGraph0Base("data\\wk5-Test-TinyDG-13-22-0base.txt", 13);
+	auto g2 = g1.Reverse();
+	auto g3 = ReadRGraph0Base("data\\wk5-Test-TinyDG-13-22-0base.txt", 13);
+	if (g2 != g3)
+	{
+		g2.Print0Base();
+		g3.Print0Base();
+		abort();
+	}
+	cout << "GraphRevDG0Base passed." << endl;
 }
 
 void GraphDFS0Base(void)
@@ -304,7 +534,7 @@ void GraphDFS0Base(void)
 }
 
 void GraphDfsOrder0Base(void)
-{
+{	
 	auto g = ReadGraph0Base("data\\wk5-Test-TinyDAG-13-15-0base.txt", 13);
 	g.Print0Base();
 
@@ -335,36 +565,206 @@ void GraphDfsOrder0Base(void)
 }
 
 
+void GraphDfsOrder(void)
+{
+	auto g = ReadGraph0Base("data\\wk5-Test-TinyDAG-13-15-0base.txt", 13);
+	g.Print();
+
+	DirectedDFS dfs(g);
+	dfs.SearchAll(g);
+	cout << "cycled: " << dfs.Cycled() << endl;
+
+	cout << "pre  : ";
+	for (auto i : dfs.Pre())
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+
+	cout << "post : ";
+	for (auto i : dfs.Post())
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+
+	cout << "rPost: ";
+	for (auto i : dfs.RevPost())
+	{
+		cout << i  << " ";
+	}
+	cout << endl;
+}
+
+
+void SccTest0Based(void)
+{
+	/*
+	revPost : 1 0 2 4 5 3 11 9 12 10 6 7 8
+	scc count 5
+	vtScc.size: 1 5 4 1 2
+	nscc: 1 5 4 1 2
+	sorted: 5 4 2 1 1
+	1
+	5 4 3 2 0
+	12 11 10 9
+	6
+	8 7
+	*/
+	auto g1 = ReadGraph0Base("data\\wk5-Test-TinyDG-13-22-0base.txt", 13);
+
+	g1.Print0Base();
+
+	auto revPost = DirectedDFS(g1).SearchAll(g1.Reverse()).RevPost();
+
+	cout << "revPost : ";
+	for (auto i : revPost)
+	{
+		cout << i - 1 << " ";
+	}
+	cout << endl;
+
+	KosarajuSCC scc(g1, revPost);
+
+	int nScc = scc.Count();
+	CHECK(nScc);
+	cout << "scc count " << nScc << endl;
+
+	vector<deque<int>> vtScc(nScc, deque<int>());
+	vector<int> vtnScc(nScc, 0);
+	for (int ii = 1; ii <= g1.V(); ++ii)
+	{
+		auto idx = scc.Idx(ii);
+		vtScc[idx].push_front(ii);
+		++vtnScc[idx];
+	}
+
+	cout << "vtScc.size: ";
+	for (auto& v : vtScc)
+	{
+		cout << v.size() << " ";
+	}
+	cout << endl;
+
+	cout << "nscc: ";
+	for (auto i : vtnScc)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+
+	sort(vtnScc.rbegin(), vtnScc.rend());
+
+	cout << "sorted: ";
+	for (auto i : vtnScc)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+
+
+	for (auto& v : vtScc)
+	{
+		for (auto i : v)
+		{
+			cout << i - 1 << " ";
+		}
+		cout << endl;
+	}
+}
+
 void GraphTest0Base(void)
 {
 	// input / output 0based, proc 1-based
+	SccTest0Based();
+	return;
 	GraphDFS0Base();
 	GraphDfsOrder0Base();
 	GraphRev0Base();
+	GraphRevDG0Base();
 }
 
-void SCC(void)
+vector<int> Top5Scc(const std::string& fn, const int nVertex)
 {
-	GraphTest0Base();
-	return;
+	auto g = ReadGraph(fn, nVertex);
 
+	auto revPost = DirectedDFS(g).SearchAll(g.Reverse()).RevPost();
 
+	cout << "revPost" << endl;
+
+	KosarajuSCC scc(g, revPost);
+	
+	auto idx = scc.Count();
+	int s = idx < 5 ? 5 : idx;
+	vector<int> vtnScc(s, 0);
+	for (auto i = 1; i <= g.V(); ++i)
+	{
+		++vtnScc[scc.Idx(i)];
+	}
+	sort(vtnScc.rbegin(), vtnScc.rend());
+	return vector<int>(vtnScc.begin(), vtnScc.begin() + 5);
+}
+
+void SccTest(void)
+{
 	class InputFile
 	{
 	public:
-		InputFile(const string& str, vector<int> res) :
-			filename(str), result(res)
+		InputFile(const string& str, int v, vector<int> res) :
+			filename(str), vertex(v), result(res)
 		{}
 		string filename;
+		int vertex;
 		vector<int> result;
 	};
 
 	vector<InputFile> vtFileNames = {
-		{ "wk5-SCC.txt", { 0, 0, 0, 0, 0 } },
-		{ "wk5-Test-3,3,1,1,0.txt", { 3, 3, 1, 1, 0 } },
-		{ "wk5-Test-3,3,2,0,0.txt", { 3, 3, 2, 0, 0 } },
-		{ "wk5-Test-3,3,3,0,0.txt", { 3, 3, 3, 0, 0 } },
-		{ "wk5-Test-6,3,2,1,0.txt", { 6, 3, 2, 1, 0 } },
-		{ "wk5-Test-7,1,0,0,0.txt", { 7, 1, 0, 0, 0 } },
+		{ "data\\wk5-SCC.txt", 875714, { 0, 0, 0, 0, 0 } },
+		{ "data\\wk5-Test-3,3,1,1,0.txt", 8, { 3, 3, 1, 1, 0 } },
+		{ "data\\wk5-Test-3,3,2,0,0.txt", 8, { 3, 3, 2, 0, 0 } },
+		{ "data\\wk5-Test-3,3,3,0,0.txt", 9, { 3, 3, 3, 0, 0 } },
+		{ "data\\wk5-Test-6,3,2,1,0.txt", 12, { 6, 3, 2, 1, 0 } },
+		{ "data\\wk5-Test-7,1,0,0,0.txt", 8, { 7, 1, 0, 0, 0 } },
 	};
+
+	for (const auto& in : vtFileNames)
+	{
+		if (in.vertex > 100)
+		{
+			cout << in.filename << " skipped" << endl;
+			continue;
+		}
+		auto res = Top5Scc(in.filename, in.vertex);
+		if (res != in.result)
+		{
+			abort();
+		}
+		cout << in.filename << endl;
+		for (auto i : res)
+		{
+			cout << i << " ";
+		}
+		cout << endl;
+	}
+
+	auto t = time(nullptr);
+	auto in = vtFileNames[0];
+	auto res = Top5Scc(in.filename, in.vertex);
+
+	cout << in.filename << endl;
+	for (auto i : res)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+	
+	t = time(nullptr) - t;
+	cout << "time cost " << t << " seconds" << endl;
+}
+
+void SCC(void)
+{
+	SccTest();
+	return;
+
 }
