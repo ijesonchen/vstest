1885d2b84c21100919d2a95e2bcbc11e8af7f19e
Jeson <jetstorm@163.com>
2018-02-22 20:02:36 +0800

    pat/a1031 0/20

--- vs2017cpp/PATTest/Advanced/A1031.cpp
+++ vs2017cpp/PATTest/Advanced/A1031.cpp
@@ -6,8 +6,11 @@ cost: 19:30
 sln1: 15min 16/20 pt2,5 WA
 sln2: n2 >= 3
 	5min  18/20 pt5 WA
-sln3: n1 < n2
+sln3: n1 + n2 + n3 - 2 = N.
 	5min pass
+
+sln3: 根据n1 + n2 + n3 - 2 = N.重新计算n1,n2
+	5min 0/20
 
 Given any string of N (>=5) characters,
 you are asked to form the characters into the shape of U.
@@ -58,6 +61,7 @@ int A1031Func(void)
 	cin >> s;
 	int n1, n2, k, len;
 	len = (int)s.length();
+
 	n1 = len / 3;
 	n2 = len - n1 * 2;
 	if (n2 < 3)
@@ -73,7 +77,7 @@ int A1031Func(void)
 		n2 = len - n1 * 2;
 	}
 	k = n2 - 2;
-	for (int i  = 0; i < n1; ++i)
+	for (int i = 0; i < n1; ++i)
 	{
 		cout << s[i];
 		for (int j = 0; j < k; ++j)
@@ -86,20 +90,42 @@ int A1031Func(void)
 	return 0;
 }
 
+int A1031Func2(void)
+{
+	string s;
+	cin >> s;
+	int n1, n2, k, len, len2;
+	len = (int)s.length();
+	len2 = len + 2;
+	n1 = len2 / 3;
+	n2 = len2 - n1 * 2;
+	k = n2 - 2;
+	for (int i = 0; i < n1; ++i)
+	{
+		cout << s[i];
+		for (int j = 0; j < k; ++j)
+		{
+			cout << " ";
+		}
+		cout << s[len - 1 - i] << endl;
+	}
+	cout << s.substr(n1, n2) << endl;
+	return 0;
+}
 
 void A1031(const string& fn)
 {
 	cout << fn << endl;
 	RedirCin(fn);
-	A1031Func();
+	A1031Func2();
 	cout << endl;
 }
 
 void A1031(void)
 {
-// 	A1031("data\\A1031-1.txt"); // 
-// 	A1031("data\\A1031-2.txt"); // 
-// 	A1031("data\\A1031-3.txt"); // 
+	A1031("data\\A1031-1.txt"); // 
+	A1031("data\\A1031-2.txt"); // 
+	A1031("data\\A1031-3.txt"); // 
 	A1031("data\\A1031-4.txt"); // 
 }
 
