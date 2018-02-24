7620e6e38bdadcc7d7896656d1602adfb592f758
Jeson <jetstorm@163.com>
2018-02-24 14:48:57 +0800

    PAT/A1135 21/30

--- vs2017cpp/PATTest/Advanced/A1135.cpp
+++ vs2017cpp/PATTest/Advanced/A1135.cpp
@@ -10,6 +10,12 @@ sln1: 仅前序无法唯一确定一棵树。应该是前序和RB树有关系。
 	猜测末尾应为 BR
 	5min 21/30 pt2,3 wa
 
+SLN2: 搜索红黑树 前序遍历，介绍红黑树的资料提到：红黑树是二叉搜索树
+	因此，满足搜索树的条件
+	前序： 根 左 右
+	二叉树前序： 根 小 大
+	70min 21/30 pt2,3 wa
+
 There is a kind of balanced binary search tree named red-black tree in the data structure. 
 It has the following 5 properties:
 
@@ -137,11 +143,183 @@ namespace nsA1135
 	}
 }
 
+namespace nsA1135B
+{
+	struct Node
+	{
+		int val = 0;
+		Node* left = nullptr;
+		Node* right = nullptr;
+
+		int black = 0;
+
+		bool IsLeaf(void) const { return !left && !right; };
+		bool Red(void) const { return val < 0; };
+		bool Black(void) const { return val > 0; };
+
+		Node(int v) : val(v) {};
+	};
+
+	vector<int> vPreOrcer;
+	vector<Node> vNode;
+
+	// [first, last]. return last + 1 if not found.
+	int RightLeftIndex(int first, int last)
+	{
+		int root = abs(vPreOrcer[first]);
+		for (int i = first + 1; i <= last; ++i)
+		{
+			if (abs(vPreOrcer[i]) > root)
+			{
+				return i;
+			}
+		}
+		return last + 1;
+	}
+
+	// [first, last]
+	void BuildTree(Node*& pRoot, int first, int last)
+	{
+		if (last < first)
+		{
+			return;
+		}
+		pRoot = &vNode[first];
+		if (first == last)
+		{
+			return;
+		}
+		// left: first+1, right-1
+		// right: right, last
+		int iLeft1 = first + 1;
+		int iRight1 = RightLeftIndex(first, last);
+		int iLeft2 = iRight1 - 1;
+		int iRight2 = last;
+		BuildTree(pRoot->left, iLeft1, iLeft2);
+		BuildTree(pRoot->right, iRight1, iRight2);
+	}
+	bool BlackChild(Node* pRoot)
+	{
+		if (!pRoot)
+		{
+			return true;
+		}
+		if (pRoot->Red())
+		{
+			if ((pRoot->left && pRoot->left->Red()) ||
+				(pRoot->right && pRoot->right->Red()))
+			{
+				return false;
+			}
+		}
+		return BlackChild(pRoot->left) && BlackChild(pRoot->right);
+	}
+
+	void SetBlackNodes(Node* pRoot)
+	{
+		if (!pRoot)
+		{
+			return;
+		}
+		int& black = pRoot->black;
+		if (pRoot->Black())
+		{
+			black++;
+		}
+		if (pRoot->left)
+		{
+			pRoot->left->black = black;
+			SetBlackNodes(pRoot->left);
+		}
+		if (pRoot->right)
+		{
+			pRoot->right->black = black;
+			SetBlackNodes(pRoot->right);
+		}
+	}
+
+	bool BlackNodes(Node* pRoot)
+	{
+		SetBlackNodes(pRoot);
+		int black = -1;
+		for (auto& n : vNode)
+		{
+			if (n.IsLeaf())
+			{
+				if (black == -1)
+				{
+					black = n.black;
+				}
+				else
+				{
+					if (black != n.black)
+					{
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+	bool IsRbTree(Node* pRoot)
+	{
+		if (!pRoot->Black())
+		{
+			return false;
+		}
+		if (!BlackChild(pRoot))
+		{
+			return false;
+		}
+		if (!BlackNodes(pRoot))
+		{
+			return false;
+		}
+		return true;
+	}
+
+	bool ReadTree(int nNode)
+	{
+		vPreOrcer.clear();
+		vNode.assign(nNode, Node(0));
+		for (int i = 0; i < nNode; ++i)
+		{
+			int t;
+			cin >> t;
+			vPreOrcer.push_back(t);
+			vNode[i].val = t;
+		}
+		Node* pRoot = nullptr;
+		int last = (int)vNode.size() - 1;
+		BuildTree(pRoot, 0, last);
+		return IsRbTree(pRoot);
+	}
+
+	void Read(void)
+	{
+		int k, n;
+		cin >> k;
+		for (int i = 0; i < k; ++i)
+		{
+			cin >> n;
+			if (ReadTree(n))
+			{
+				cout << "Yes" << endl;
+			}
+			else
+			{
+				cout << "No" << endl;
+			}
+		}
+	}
+}
+
 
 // rename this to main int PAT
 int A1135Func(void)
 {
-	nsA1135::Read();
+	nsA1135B::Read();
 	return 0;
 }
 
