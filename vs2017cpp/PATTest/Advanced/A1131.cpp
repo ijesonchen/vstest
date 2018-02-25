7eec1c00998f62eec71d2f9ccbdbc08bd121f933
Jeson <jetstorm@163.com>
2018-02-25 23:08:55 +0800

    PAT/A1131 7/30
    refactor one dfs pass

--- vs2017cpp/PATTest/Advanced/A1131.cpp
+++ vs2017cpp/PATTest/Advanced/A1131.cpp
@@ -22,6 +22,12 @@ sln1:
 	k：查询数 1-10
 	2h pass
 
+sln: ref: luochuo
+	通过一遍DFS减枝搜索路径
+	将nsA1131修改为
+	nsA1131RefOneDfs
+	10min 7/30 pt0,2,3 WA
+	
 
 In the big cities, the subway systems always look so complex to the visitors. 
 To give you some sense, the following figure shows the map of Beijing subway.
@@ -282,10 +288,268 @@ namespace nsA1131A
 	}
 }
 
+// ref: luochuo
+// 修改为只用一遍DFS搜索路径
+namespace nsA1131RefOneDfs
+{
+	typedef unordered_map<int, int> MapIntInt;
+
+
+	const int MAXNODE = 10000;
+	vector<vector<int>> vAdjencents;
+	MapIntInt mapEdge2Line;
+
+	// for search
+	int dstNode = 0;
+	int minDist = MAXNODE;
+	int minTrans = MAXNODE;
+	vector<int> vPathNode;
+	vector<int> vPathTemp;
+	vector<bool> vVisit;
+
+	int EdgeId(int s1, int s2) { return (s1 << 16) + s2; }
+	int EdgeSrc(int e) { return e >> 16; }
+	int EdgeDst(int e) { return e & 0xFFFF; }
+
+	void Read()
+	{
+		vAdjencents.assign(MAXNODE, vector<int>());
+		mapEdge2Line.clear();
+
+		int n, m;
+		cin >> n;
+		for (int i = 0; i < n; ++i)
+		{
+			int line = i + 1;
+			int s1, s2;
+			cin >> m >> s1;
+			for (int j = 1; j < m; ++j)
+			{
+				cin >> s2;
+				vAdjencents[s1].push_back(s2);
+				vAdjencents[s2].push_back(s1);
+				mapEdge2Line[EdgeId(s1, s2)] = line;
+				mapEdge2Line[EdgeId(s2, s1)] = line;
+				s1 = s2;
+			}
+		}
+	}
+
+	void Dfs(int from, int dist, int trans, int line)
+	{
+// 		if (dist > vDist[from])
+// 		{
+// 			return;
+// 		}
+		if (dist > minDist)
+		{
+			return;
+		}
+		if (from == dstNode)
+		{
+			// found path
+			if (dist <= minDist && trans < minTrans)
+			{
+				minDist = dist;
+				minTrans = trans;
+				vPathNode = vPathTemp;
+			}
+			return;
+		}
+		vector<int>& vAdj = vAdjencents[from];
+		for (auto next : vAdj)
+		{
+			if (vVisit[next])
+			{
+				continue;
+			}
+			int nextTrans = trans;
+			int nextLine = mapEdge2Line[EdgeId(from, next)];
+			if (line != -1 && line != nextLine)
+			{
+				++nextTrans;
+			}
+			vVisit[next] = true;
+			vPathTemp.push_back(next);
+			Dfs(next, dist + 1, nextTrans, nextLine);
+			vVisit[next] = false;
+			vPathTemp.pop_back();
+		}
+	}
+
+	void PrintLine(int line, int from, int to)
+	{
+		// Take Line#3 from 3011 to 3013.
+		printf("Take Line#%d from %04d to %04d.\n", line, from, to);
+	}
+
+	void PrintPath(void)
+	{
+		size_t len = vPathNode.size();
+		cout << len - 1 << endl;
+		int from = vPathNode[0];
+		int to = vPathNode[1];
+		int line1 = mapEdge2Line[EdgeId(from, to)];
+		for (int i = 2; i < len; ++i)
+		{
+			int next = vPathNode[i];
+			int line2 = mapEdge2Line[EdgeId(to, next)];
+			if (line2 != line1)
+			{
+				PrintLine(line1, from, to);
+				from = to;
+				line1 = line2;
+			}
+			to = next;
+		}
+		PrintLine(line1, from, to);
+	}
+
+	void ResetDfs(int src, int dst)
+	{
+		dstNode = dst;
+		minDist = MAXNODE;
+		minTrans = MAXNODE;
+		vPathNode.clear();
+		vPathTemp.clear();
+		vVisit.assign(MAXNODE, false);
+		vVisit[src] = true;
+		vPathTemp.push_back(src);
+	}
+
+	void Search(int s1, int s2)
+	{
+		ResetDfs(s1, s2);
+		Dfs(s1, 0, 0, -1);
+		PrintPath();
+	}
+
+	void Query(void)
+	{
+		int k, s1, s2;
+		cin >> k;
+		for (int i = 0; i < k; ++i)
+		{
+			cin >> s1 >> s2;
+			Search(s1, s2);
+		}
+	}
+
+	void main(void)
+	{
+		Read();
+		Query();
+	}
+}
+
+// https://www.liuchuo.net/archives/3850
+// 通过一边DFS减枝实现路径搜索。
+namespace nsA1131LiuchuoRef
+{
+	// 路径邻接链表
+	vector<vector<int>> v(10000);
+
+	// 边-线路映射
+	int line[10000][10000];
+	int	visit[10000], 
+		minCnt, 
+		minTransfer, 
+		start, 
+		end1;
+	vector<int> path, tempPath;
+
+	int transferCnt(vector<int> a) 
+	{
+		int cnt = -1, preLine = 0;
+		for (int i = 1; i < a.size(); i++) 
+		{
+			if (line[a[i - 1]][a[i]] != preLine)
+			{
+				cnt++;
+			}
+			preLine = line[a[i - 1]][a[i]];
+		}
+		return cnt;
+	}
+
+	void dfs(int node, int cnt) 
+	{
+		if (node == end1 && 
+			(cnt < minCnt || 
+				(cnt == minCnt && 
+					transferCnt(tempPath) < minTransfer))) 
+		{
+			minCnt = cnt;
+			minTransfer = transferCnt(tempPath);
+			path = tempPath;
+		}
+		if (node == end1) 
+		{
+			return;
+		}
+		for (int i = 0; i < v[node].size(); i++) 
+		{
+			if (visit[v[node][i]] == 0) 
+			{
+				visit[v[node][i]] = 1;
+				tempPath.push_back(v[node][i]);
+				dfs(v[node][i], cnt + 1);
+				visit[v[node][i]] = 0;
+				tempPath.pop_back();
+			}
+		}
+	}
+
+	int main() {
+		int n, m, k, pre, temp;
+		scanf("%d", &n);
+		for (int i = 0; i < n; i++) 
+		{
+			scanf("%d%d", &m, &pre);
+			for (int j = 1; j < m; j++) 
+			{
+				scanf("%d", &temp);
+				v[pre].push_back(temp);
+				v[temp].push_back(pre);
+				line[pre][temp] = line[temp][pre] = i + 1;
+				pre = temp;
+			}
+		}
+
+		scanf("%d", &k);
+		for (int i = 0; i < k; i++) 
+		{
+			scanf("%d%d", &start, &end1);
+			minCnt = 99999, minTransfer = 99999;
+			tempPath.clear();
+			tempPath.push_back(start);
+			visit[start] = 1;
+			dfs(start, 0);
+			visit[start] = 0;
+			printf("%d\n", minCnt);
+			int preLine = 0, preTransfer = start;
+			for (int j = 1; j < path.size(); j++) 
+			{
+				if (line[path[j - 1]][path[j]] != preLine) 
+				{
+					if (preLine != 0) 
+					{
+						printf("Take Line#%d from %04d to %04d.\n", preLine, preTransfer, path[j - 1]);
+					}
+					preLine = line[path[j - 1]][path[j]];
+					preTransfer = path[j - 1];
+				}
+			}
+			printf("Take Line#%d from %04d to %04d.\n", preLine, preTransfer, end1);
+		}
+		return 0;
+	}
+}
+
 // rename this to main int PAT
 int A1131Func(void)
 {
-	nsA1131A::main();
+	nsA1131RefOneDfs::main();
 	return 0;
 }
 
