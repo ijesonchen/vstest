9183f5eddc4e1ec411aecd6fec2e649667cfeecb
Jeson <jetstorm@163.com>
2018-03-08 12:13:42 +0800

    PAT/A1033 FAIL A1034 28/30

--- vs2017cpp/PATTest/Advanced/A1033.cpp
+++ vs2017cpp/PATTest/Advanced/A1033.cpp
@@ -1,16 +1,160 @@
 /*
+1033. To Fill or Not to Fill (25)
+100 ms
+65536 kB
 
+9:50
+
+
+With highways available, driving a car from Hangzhou to any other city is easy. 
+But since the tank capacity of a car is limited, we have to find gas stations on the way from time to time. 
+Different gas station may give different price. You are asked to carefully design the cheapest route to go.
+
+Input Specification:
+
+Each input file contains one test case.
+For each case, the first line contains 4 positive numbers: 
+Cmax (<= 100), the maximum capacity of the tank; 
+D (<=30000), the distance between Hangzhou and the destination city; 
+Davg (<=20), the average distance per unit gas that the car can run; and 
+N (<= 500), the total number of gas stations. 
+
+Then N lines follow, each contains a pair of non-negative numbers:
+Pi, the unit gas price, and Di (<=D), the distance between this station and Hangzhou, 
+for i=1,...N. All the numbers in a line are separated by a space.
+
+Output Specification:
+
+For each test case, print the cheapest price in a line, accurate up to 2 decimal places. 
+It is assumed that the tank is empty at the beginning.
+If it is impossible to reach the destination, print "The maximum travel distance = X" 
+where X is the maximum possible distance the car can run, accurate up to 2 decimal places.
+
+Sample Input 1:
+50 1300 12 8
+6.00 1250
+7.00 600
+7.00 150
+7.10 0
+7.20 200
+7.50 400
+7.30 1000
+6.85 300
+Sample Output 1:
+749.17
+Sample Input 2:
+50 1300 12 2
+7.10 0
+7.00 600
+Sample Output 2:
+The maximum travel distance = 1200.00
 */
 
 #include "..\patMain.h"
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+
+/*
+9:50
+起始油量为空
+正数：cmax油箱容量100] D 目标距离30000] Davg单位油距离20] N油站数量500]
+非负：pi油价 di起点距离
+
+.2f 最低价格，或无法到达时最大距离
+
+动态规划？递归
+最多加油30000/20=1500
+最加50次油
+每次加油最多distTank=cmax*davg，要小于各油站间距：无法到达时的处理
+每次价格p[i]，剩余油量gleft[i]，加油量gadd[i]，可用续航dgas[i]，总价total[i]
+
+贪心：
+增加目的加油站，油价为0
+当前开始（含），
+当前油箱覆盖的站点
+最大油箱距离能覆盖的油站
+11：00 放弃
+*/
+
+namespace nsA1033A
+{
+	struct Station 
+	{
+		float dist = 0;
+		float p = 0;
+
+		bool operator<(const Station& s)
+		{ return dist != s.dist ? dist < s.dist : p < s.p; }
+	};
+
+
+
+	void main(void)
+	{
+		int n;
+		float cMax, dist, davg;
+		scanf("%f %f %f %d", &cMax, &dist, &davg, &n);
+		vector<Station> vStation;
+		for (int i = 0; i < n; ++i)
+		{
+			vStation.emplace_back();
+			scanf("%f %f", &vStation.back().p, &vStation.back().dist);
+		}
+		vStation.emplace_back();
+		vStation.back().p = 0; vStation.back().dist = dist;
+		sort(vStation.begin(), vStation.end());
+		if (vStation.front().dist != 0)
+		{
+			printf("The maximum travel distance = 0.00\n");
+			return;
+		}
+		bool bImpossible = false;
+
+		float pTotal = 0;
+		float gLeft = 0;
+		Station* pPrev = &vStation.front();
+		float gPrice = pPrev->p;
+		for (size_t i = 1; i < vStation.size(); ++i)
+		{
+			Station& next = vStation[i];
+			if (next.dist >= dist)
+			{
+				break;
+			}
+			float gCost = (next.dist - pPrev->dist) / davg;
+			if (gCost > cMax)
+			{
+				bImpossible = true;
+				break;
+			}
+			float gNeed;
+			if (pPrev->p < next.p) {  gNeed = cMax; } // fufill
+			else { gNeed =  gCost; }
+			pTotal = gPrice * (gNeed - gLeft);
+			gPrice = next.p;
+			gLeft = gNeed - gCost;
+			pPrev = &next;
+		}
+		if (bImpossible)
+		{
+			float totolDist = pPrev->dist + gLeft * davg;
+			printf("The maximum travel distance = %.2f\n", totolDist);
+		}
+		else
+		{
+			printf("%.2f\n", pTotal);
+		}
+	}
+}
 
 // rename this to main int PAT
 int A1033Func(void)
 {
+	nsA1033A::main();
 	return 0;
 }
 
@@ -26,5 +170,6 @@ void A1033(const string& fn)
 void A1033(void)
 {
 	A1033("data\\A1033-1.txt"); // 
+	A1033("data\\A1033-2.txt"); // 
 }
 
--- vs2017cpp/PATTest/Advanced/A1034.cpp
+++ vs2017cpp/PATTest/Advanced/A1034.cpp
@@ -1,16 +1,205 @@
 /*
+1034. Head of a Gang (30)
+100 ms
+65536 kB
 
+One way that the police finds the head of a gang is to check people's phone calls. 
+If there is a phone call between A and B, we say that A and B is related. 
+The weight of a relation is defined to be the total time length of all the phone calls made between the two persons. 
+A "Gang" is a cluster of more than 2 persons 
+who are related to each other with total relation weight being greater than a given threshold K. 
+In each gang, the one with maximum total weight is the head. 
+Now given a list of phone calls, you are supposed to find the gangs and the heads.
+
+Input Specification:
+
+Each input file contains one test case. For each case, 
+the first line contains two positive numbers N and K (both less than or equal to 1000), 
+the number of phone calls and the weight threshold, respectively. 
+Then N lines follow, each in the following format:
+
+Name1 Name2 Time
+
+where Name1 and Name2 are the names of people at the two ends of the call, 
+and Time is the length of the call. 
+A name is a string of three capital letters chosen from A-Z. 
+A time length is a positive integer which is no more than 1000 minutes.
+
+Output Specification:
+
+For each test case, first print in a line the total number of gangs. 
+Then for each gang, print in a line the name of the head and the total number of the members. 
+It is guaranteed that the head is unique for each gang. 
+The output must be sorted according to the alphabetical order of the names of the heads.
+
+Sample Input 1:
+8 59
+AAA BBB 10
+BBB AAA 20
+AAA CCC 40
+DDD EEE 5
+EEE DDD 70
+FFF GGG 30
+GGG HHH 20
+HHH FFF 10
+Sample Output 1:
+2
+AAA 3
+GGG 3
+Sample Input 2:
+8 70
+AAA BBB 10
+BBB AAA 20
+AAA CCC 40
+DDD EEE 5
+EEE DDD 70
+FFF GGG 30
+GGG HHH 20
+HHH FFF 10
+Sample Output 2:
+0
 */
 
 #include "..\patMain.h"
 #include <iostream>
 #include <string>
+#include <vector>
+#include <unordered_map>
+#include <deque>
+#include <algorithm>
+
 
 using namespace std;
+
+/*
+11£º05
+12:10: 28/30 PT3 ¶Î´íÎó
+*/
+
+namespace nsA1034A
+{
+//	const int MAXNAME = 26 * 26 * 26; // 20K
+	int Name2ID(const char* p)
+	{
+		return (p[0] - 'A')* 26*26+(p[1] - 'A')*26+(p[2] - 'A');
+	}
+
+	char* ID2Name(int n)
+	{
+		static char name[4];
+		name[0] = n / (26 * 26) + 'A';
+		name[1] = n / 26 % 26 + 'A';
+		name[2] = n % (26) + 'A';
+		name[3] = 0;
+		return name;
+	}
+
+	struct Result
+	{
+		int name = 0;
+		int size = 0;
+
+		bool operator < (const Result& a)
+		{
+			return name < a.name;
+		}
+	};
+
+
+
+	void main(void)
+	{
+		int n, k, t;
+		char name1[4], name2[4];
+		scanf("%d %d", &n, &k);
+		k *= 2;
+		vector<int> vWeight(1);
+		vector<int> vName(1);
+		vector<vector<int>> vvAdj(n+2); // max n+2 people
+		unordered_map<int, int> mapNameIndex;
+		for (int i = 0; i < n; ++i)
+		{
+			scanf("%s %s %d", name1, name2, &t);
+			int id1 = Name2ID(name1);
+			int id2 = Name2ID(name2);
+			int& idx1 = mapNameIndex[id1];
+			if (!idx1)
+			{
+				idx1 = (int)vWeight.size();
+				vWeight.push_back(0);
+				vName.push_back(id1);
+			}
+			int& idx2 = mapNameIndex[id2];
+			if (!idx2)
+			{
+				idx2 = (int)vWeight.size();
+				vWeight.push_back(0);
+				vName.push_back(id2);
+			}
+			vWeight[idx1] += t;
+			vWeight[idx2] += t;
+			vvAdj[idx1].push_back(idx2);
+			vvAdj[idx2].push_back(idx1);
+		}
+		vector<Result> vResult;
+		vector<bool> vVisit(vWeight.size());
+		vVisit[0] = true;
+		for (size_t i = 0; i < vVisit.size(); ++i)
+		{
+			if (vVisit[i])
+			{
+				continue;
+			}
+			vVisit[i] = true;
+			int idMax = (int)i;
+			int totalWeight = vWeight[idMax];
+			int maxWeight = vWeight[idMax];
+			int nMember = 1;
+			deque<int> dqNodes;
+			dqNodes.push_back(idMax);
+			while (!dqNodes.empty())
+			{
+				int u = dqNodes.front();
+				dqNodes.pop_front();
+				vector<int>& uAdj = vvAdj[u];
+				for (auto v : uAdj)
+				{
+					if (vVisit[v])
+					{
+						continue;
+					}
+					++nMember;
+					vVisit[v] = true;
+					dqNodes.push_back(v);
+					totalWeight += vWeight[v];
+					if (vWeight[v] > maxWeight)
+					{
+						idMax = v;
+						maxWeight = vWeight[idMax];
+					}
+				}
+			}
+			if ((totalWeight > k) && (nMember > 2))
+			{
+				vResult.emplace_back();
+				vResult.back().name = vName[idMax];
+				vResult.back().size = nMember;
+			}			
+		}
+		sort(vResult.begin(), vResult.end());
+		printf("%d\n", (int)vResult.size());
+		for (auto& res : vResult)
+		{
+			printf("%s %d\n", ID2Name(res.name), res.size);
+		}
+		return;
+	}
+}
 
 // rename this to main int PAT
 int A1034Func(void)
 {
+	nsA1034A::main();
 	return 0;
 }
 
@@ -25,6 +214,7 @@ void A1034(const string& fn)
 
 void A1034(void)
 {
-	A1034("data\\A1034-1.txt"); // 
+//	A1034("data\\A1034-1.txt"); // 
+	A1034("data\\A1034-2.txt"); // 
 }
 
